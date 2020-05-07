226e8aca9e51b823eaad0f8f79924f5caa990700
bhavin <bjacc03@gmail.com>
2020-05-07 15:11:11 +0530

    //commit

--- CommanFiles/ViewController.swift
+++ CommanFiles/ViewController.swift
@@ -7,61 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
+import BraintreeDropIn
+import Braintree
 
-class ViewController: UIViewController {
-    var range = NSRange()
-    @IBOutlet weak var lbldata: UILabel!
+class ViewController: UIViewController{
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.callapi()
+        
         // Do any additional setup after loading the view.
     }
     
-    func callapi(){
-         let parameters = ["lng":"en","type":"gold"]
-        AFWrapper.postMethod(params: parameters as [String : AnyObject], apikey: "http://3.0.203.214/api/web/tumi/users/membershipprivilages", completion: { (json) in
-            let dict = json as! NSDictionary
-            
-            if dict["status"] as? Int == 1
-            {
-                DispatchQueue.main.async{
-                    
-                    let dataDict = dict.value(forKey: "data") as! NSDictionary
-                    let strContent = dataDict.value(forKey: "text") as! String
-                    let modifiedFont = NSString(format:"<span style=\"font-family: \(UIFont.systemFont(ofSize: 15)); font-size: \(15)\">%@</span>" as NSString, strContent)
-                    let encodedData = modifiedFont.data(using: String.Encoding.utf8.rawValue)!
-                    
-                    var attributedString: NSAttributedString
-                    
-                    do {
-                        attributedString = try NSAttributedString(data: encodedData, options: [NSAttributedString.DocumentReadingOptionKey.documentType:NSAttributedString.DocumentType.html,NSAttributedString.DocumentReadingOptionKey.characterEncoding:NSNumber(value: String.Encoding.utf8.rawValue)], documentAttributes: nil)
-                        
-                        attributedString.enumerateAttribute(NSAttributedString.Key.link , in: NSMakeRange(0, attributedString.length), options: [.longestEffectiveRangeNotRequired]) { value, range, isStop in
-                            if let value = value {
-                                print("\(value) found at \(range.location)")
-                                self.range = NSRange(location: range.location - 50, length: range.length + 50)
-                            }
-                        }
-                        
-                        self.lbldata.font = UIFont(name: Constants.FONT, size: 16)
-                        self.lbldata?.attributedText = attributedString
-                        
-                    } catch let error as NSError {
-                        print(error.localizedDescription)
-                    } catch {
-                        print("error")
-                    }
-                    self.lbldata?.numberOfLines = 0
-                }
-            }
-            
-
-        }, failure: { (error) in
-            
-            print(error)
-        })
+    @IBAction func btnPaypalclick(_ sender: Any) {
+        //self.showDropIn(clientTokenOrTokenizationKey: "sandbox_7bcz8zq2_t42yhd2svwczn7zh")
+        self.showDropIn(clientTokenOrTokenizationKey: "eyJ2ZXJzaW9uIjoyLCJlbnZpcm9ubWVudCI6InNhbmRib3giLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJleUowZVhBaU9pSktWMVFpTENKaGJHY2lPaUpGVXpJMU5pSXNJbXRwWkNJNklqSXdNVGd3TkRJMk1UWXRjMkZ1WkdKdmVDSXNJbWx6Y3lJNklrRjFkR2g1SW4wLmV5SmxlSEFpT2pFMU9EZzVNakUwTkRFc0ltcDBhU0k2SWpFeE56TmhOVGxqTFRnMFpqRXROREE1TVMwNFlUSXdMV05rWVRjM1lXUmlZbVZrTVNJc0luTjFZaUk2SWpkemVHSnVkMmQ0YUdaM2FIWm1NM0VpTENKcGMzTWlPaUpCZFhSb2VTSXNJbTFsY21Ob1lXNTBJanA3SW5CMVlteHBZMTlwWkNJNklqZHplR0p1ZDJkNGFHWjNhSFptTTNFaUxDSjJaWEpwWm5sZlkyRnlaRjlpZVY5a1pXWmhkV3gwSWpwbVlXeHpaWDBzSW5KcFoyaDBjeUk2V3lKdFlXNWhaMlZmZG1GMWJIUWlYU3dpYjNCMGFXOXVjeUk2ZTMxOS5qbUtYYm5wWi1iT2gwNkdXR1Npd2JUOXpubzE2TnNnRzBLVi1taXlaLTVOQUI3MU5JMTdWeVpyZEdvMjBJRlo5TUZTZEdDNUdoZVVESTR5RnczRTRhdyIsImNvbmZpZ1VybCI6Imh0dHBzOi8vYXBpLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb206NDQzL21lcmNoYW50cy83c3hibndneGhmd2h2ZjNxL2NsaWVudF9hcGkvdjEvY29uZmlndXJhdGlvbiIsImdyYXBoUUwiOnsidXJsIjoiaHR0cHM6Ly9wYXltZW50cy5zYW5kYm94LmJyYWludHJlZS1hcGkuY29tL2dyYXBocWwiLCJkYXRlIjoiMjAxOC0wNS0wOCJ9LCJjaGFsbGVuZ2VzIjpbXSwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzdzeGJud2d4aGZ3aHZmM3EvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vb3JpZ2luLWFuYWx5dGljcy1zYW5kLnNhbmRib3guYnJhaW50cmVlLWFwaS5jb20vN3N4Ym53Z3hoZndodmYzcSJ9LCJ0aHJlZURTZWN1cmVFbmFibGVkIjp0cnVlLCJwYXlwYWxFbmFibGVkIjp0cnVlLCJwYXlwYWwiOnsiZGlzcGxheU5hbWUiOiJSZWRzcGFyayIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJtZXJjaGFudEFjY291bnRJZCI6InJlZHNwYXJrIiwiY3VycmVuY3lJc29Db2RlIjoiVVNEIn0sIm1lcmNoYW50SWQiOiI3c3hibndneGhmd2h2ZjNxIiwidmVubW8iOiJvZmYifQ==")
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    func showDropIn(clientTokenOrTokenizationKey: String) {
+        let request =  BTDropInRequest()
+        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
+        { (controller, result, error) in
+            if (error != nil) {
+                print("ERROR")
+            } else if (result?.isCancelled == true) {
+                print("CANCELLED")
+            } else if let result = result {
+                // Use the BTDropInResult properties to update your UI
+                let selectedPaymentOptionType = result.paymentOptionType
+                let selectedPaymentMethod = result.paymentMethod
+                let selectedPaymentMethodIcon = result.paymentIcon
+                let selectedPaymentMethodDescription = result.paymentDescription
+                
+                let out = result.paymentMethod!
+                print("token------",out.nonce)
 
+                
+            }
+            controller.dismiss(animated: true, completion: nil)
+        }
+        self.present(dropIn!, animated: true, completion: nil)
+    }
+    
+ 
 
 }
-
