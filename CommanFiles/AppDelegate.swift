86e3ccb38b4737cd0693a5ffaf11a0c82768a3e5
bhavin <bjacc03@gmail.com>
2020-05-08 14:37:41 +0530

    latest

--- CommanFiles/AppDelegate.swift
+++ CommanFiles/AppDelegate.swift
@@ -161,5 +161,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         
     }
+    
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        if url.scheme?.localizedCaseInsensitiveCompare("bhavin.scandemo.payments") == .orderedSame {
+            return BTAppSwitch.handleOpen(url, options: options)
+        }
+        return false
+    }
+
+    // If you support iOS 8, add the following method.
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        if url.scheme?.localizedCaseInsensitiveCompare("bhavin.scandemo.payments") == .orderedSame {
+            return BTAppSwitch.handleOpen(url, sourceApplication: sourceApplication)
+        }
+        return false
+    }
 }
 
